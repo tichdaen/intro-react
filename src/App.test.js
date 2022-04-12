ef63d5ab1ba3d19d832703ce615cc9e7962c9e9b
jeondaen <jeondaen@gmail.com>
2022-04-13 01:12:24 +0900

    Add jest and some Test Code

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render, screen } from '@testing-library/react'
+import userEvent from "@testing-library/user-event";
+import List from "./List";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+function addItem(item) {
+  arr.push(item);
+}
+
+const arr = ['Lee', "Holand"];
+
+describe('React App Test', () => {
+
+  it('List Test', async () => {
+    const component = <List addFunction={addItem} title="title" placeholder='somethin' currList={arr} />;
+    const {debug} = render(component);
+
+    const inputs = screen.getByPlaceholderText('somethin')
+    const submit = screen.getByText('Submit');
+    
+    userEvent.type(inputs, 'Daen');
+    userEvent.click(submit);
+    
+    debug();
+  })
+
+})
